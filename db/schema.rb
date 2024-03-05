a7919432e9e98abfee9ed2e793204f52368f5daf
Charlize Santos <charlize-santos@uiowa.edu>
2024-03-04 21:03:57 -0600

    Working progress bar with previous question and color change

--- db/schema.rb
+++ db/schema.rb
@@ -1,90 +1,82 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20240215211302) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_02_15_211302) do
   create_table "groups", force: :cascade do |t|
-    t.string  "name"
-    t.string  "description"
-    t.string  "join_token"
+    t.string "name"
+    t.string "description"
+    t.string "join_token"
     t.integer "owner"
   end
 
   create_table "groups_users", id: false, force: :cascade do |t|
     t.integer "group_id", null: false
-    t.integer "user_id",  null: false
+    t.integer "user_id", null: false
+    t.index ["group_id", "user_id"], name: "index_groups_users_on_group_id_and_user_id"
   end
-
-  add_index "groups_users", ["group_id", "user_id"], name: "index_groups_users_on_group_id_and_user_id"
 
   create_table "quiz_sounds", force: :cascade do |t|
     t.integer "quiz_id"
     t.integer "sound_id"
+    t.index ["quiz_id", "sound_id"], name: "index_quiz_sounds_on_quiz_id_and_sound_id", unique: true
   end
-
-  add_index "quiz_sounds", ["quiz_id", "sound_id"], name: "index_quiz_sounds_on_quiz_id_and_sound_id", unique: true
 
   create_table "quizzes", force: :cascade do |t|
-    t.datetime "created_at",         null: false
-    t.integer  "user_id"
-    t.string   "which_grbas_letter"
-    t.text     "quiz_answers"
-    t.integer  "difficulty"
-    t.integer  "num_questions"
+    t.datetime "created_at", null: false
+    t.integer "user_id"
+    t.string "which_grbas_letter"
+    t.text "quiz_answers"
+    t.integer "difficulty"
+    t.integer "num_questions"
+    t.index ["user_id"], name: "index_quizzes_on_user_id"
   end
-
-  add_index "quizzes", ["user_id"], name: "index_quizzes_on_user_id"
 
   create_table "quizzes_responses", id: false, force: :cascade do |t|
-    t.integer "quiz_id",     null: false
+    t.integer "quiz_id", null: false
     t.integer "response_id", null: false
+    t.index ["quiz_id", "response_id"], name: "index_quizzes_responses_on_quiz_id_and_response_id"
   end
-
-  add_index "quizzes_responses", ["quiz_id", "response_id"], name: "index_quizzes_responses_on_quiz_id_and_response_id"
 
   create_table "quizzes_users", id: false, force: :cascade do |t|
     t.integer "quiz_id", null: false
     t.integer "user_id", null: false
+    t.index ["quiz_id", "user_id"], name: "index_quizzes_users_on_quiz_id_and_user_id"
+    t.index ["user_id", "quiz_id"], name: "index_quizzes_users_on_user_id_and_quiz_id"
   end
-
-  add_index "quizzes_users", ["quiz_id", "user_id"], name: "index_quizzes_users_on_quiz_id_and_user_id"
-  add_index "quizzes_users", ["user_id", "quiz_id"], name: "index_quizzes_users_on_user_id_and_quiz_id"
 
   create_table "responses", force: :cascade do |t|
-    t.integer  "rating"
-    t.text     "reasoning"
-    t.text     "feedback"
+    t.integer "rating"
+    t.text "reasoning"
+    t.text "feedback"
     t.datetime "created_at"
-    t.integer  "quiz_id"
-    t.integer  "sound_id"
-    t.integer  "user_id"
+    t.integer "quiz_id"
+    t.integer "sound_id"
+    t.integer "user_id"
+    t.index ["quiz_id"], name: "index_responses_on_quiz_id"
+    t.index ["sound_id"], name: "index_responses_on_sound_id"
+    t.index ["user_id"], name: "index_responses_on_user_id"
   end
-
-  add_index "responses", ["quiz_id"], name: "index_responses_on_quiz_id"
-  add_index "responses", ["sound_id"], name: "index_responses_on_sound_id"
-  add_index "responses", ["user_id"], name: "index_responses_on_user_id"
 
   create_table "sounds", force: :cascade do |t|
     t.integer "sound_id"
-    t.string  "db_file_name"
-    t.text    "score_explanation"
-    t.text    "hint"
-    t.float   "g_rating"
-    t.float   "r_rating"
-    t.float   "b_rating"
-    t.float   "a_rating"
-    t.float   "s_rating"
-    t.text    "audio_file_path"
+    t.string "db_file_name"
+    t.text "score_explanation"
+    t.text "hint"
+    t.float "g_rating"
+    t.float "r_rating"
+    t.float "b_rating"
+    t.float "a_rating"
+    t.float "s_rating"
+    t.text "audio_file_path"
   end
 
   create_table "users", force: :cascade do |t|
