c6cfc73f43b7cd223da87a537852f42097580f58
kmackin <kailey-mackin@uiowa.edu>
2024-04-24 12:45:52 -0500

    updated sessions controller tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -16,6 +16,14 @@ class SessionsControllerSpec
       post :create, params: {session: {email: @user.email, password: "Password!1"}}
       expect(session["session_token"]).to eq(@session_token)
       end
+      it "should pick up a session for a logged in individual" do
+        get :new
+        @user = User.new({:email => "testuser50000@gmail.com", :password => "Password!1", :password_confirmation => "Password!1", :name => "Student1",:music_experience => "0", :clinical_experience => "1-2", :general_education => "0", :access_level => "Student", :new_user => true})
+        @user.update(new_user: true)
+        expect(@user.new_user).to be true
+        post :create, params: {session: {email: @user.email, password: "Password!1"}}
+        puts flash[:notice]
+      end
       it "should not create a session if the user does not exist" do
         @user = User.find_by_email("testuser@gmail.com")
         @session_token = @user.session_token
@@ -30,14 +38,12 @@ class SessionsControllerSpec
       delete :destroy
       expect(flash[:notice]).to eq("Successfully logged out")
       end
+      it "should delete a session if a user logs out & change their status to no longer be a new user" do
+        @user = User.new({:email => "testuser1@gmail.com", :password => "Password!1", :password_confirmation => "Password!1", :name => "Student1",:music_experience => "0", :clinical_experience => "1-2", :general_education => "0", :access_level => "Student", :new_user => true})
+        controller.instance_variable_set(:@current_user, @user)
+        delete :destroy
+        expect(flash[:notice]).to eq("Successfully logged out")
+      end
     end
   end
-    # context 'with valid authentication' do
-    #   before do
-    #     allow(request.env['omniauth.auth']).to receive(:[]).with('provider').and_return(auth_hash['provider'])
-    #     allow(request.env['omniauth.auth']).to receive(:[]).with('uid').and_return(auth_hash['uid'])
-    #     allow(User).to receive(:find_by_provider_and_uid).with(auth_hash['provider'], auth_hash['uid']).and_return(user)
-    #     allow(User).to receive(:create_with_omniauth).with(auth_hash).and_return(user)
-    #   end
-
 end
