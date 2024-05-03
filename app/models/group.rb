90a6f32739e35c7f71e9ac756857927bbb93a8e5
kmackin <kailey-mackin@uiowa.edu>
2024-05-02 23:20:16 -0500

    group documentation

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,10 +4,17 @@ class Group  < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
   validates :owner, presence: true
+
+  # Defines the ability to be added to a group; NOT the owner, just people in the group
   has_and_belongs_to_many :users, :join_table => "groups_users"
 
   before_save :generate_join_token
 
+  # Function: generate_join_token creates the join token for the group which will be used to add users
+  # to the group. Uses a simple SecureRandom.hex to establish a basic hexadecimal string
+  # to be input by users. Also ensures that the join token is unique by checking any created
+  # join tokens against all previous created ones.
+  # Return type: None; saves the join token to the group
   def generate_join_token
     join_token = SecureRandom.hex(4)
     if Group.exists?(join_token: join_token)
@@ -17,10 +24,17 @@ class Group  < ActiveRecord::Base
     end
   end
 
+  # Function: owner? determines whether or not the current user is a owner of a group
+  # by checking the user id of the owner against the current user's id.
+  # Return type: bool for if the user is an owner
   def owner?(user)
     user.id == owner
   end
 
+  # Function: find_group? finds a group based on the join token associated to that group.
+  # If the token exists and is associated to a group then it returns the group
+  # otherwise it will return a false value indicating no group could be found
+  # Return type: Either group or false dependent on whether or not the group exists
   def self.find_group(join_token)
     if Group.exists?(join_token: join_token)
       @group = Group.where(join_token: join_token).first
@@ -29,6 +43,12 @@ class Group  < ActiveRecord::Base
     end
   end
 
+  # Function: get_group_quiz_grades retrieves all the group statistics for quizzes
+  # first it finds all the users associated to a group and
+  # calls the user method "get_individual_quiz_grades" then adds
+  # the individual quiz stats to a hashmap containing the group's
+  # total stats
+  # Return type: Hashmap [ "letter from GRBAS" => [num_right, total_num_questions]]
   def get_group_quiz_grades
     # Use the user methods get individual quiz grades and add them up
     users = self.users
@@ -60,6 +80,12 @@ class Group  < ActiveRecord::Base
     final_hash
   end
 
+  # Function: get_group_test_grades retrieves all the group statistics for tests
+  # first it finds all the users associated to a group and
+  # calls the user method "get_individual_test_grades" then adds
+  # the individual test stats to an array containing the group's
+  # total stats
+  # Return type: Array [num_right, total_num_questions]
   def get_group_test_grades
     users = self.users
     total_test_scores = [0,0]
@@ -72,6 +98,13 @@ class Group  < ActiveRecord::Base
     total_test_scores
   end
 
+  # Function: get_demographic_stats gets the demographic data for all users in a group and
+  # returns the average experience in years for each individual category of demographic data.
+  # First it finds all the users in a group, then it retrieves their demographic data (stored as an array)
+  # and adds it to the group demographic data for the total experience for each individual stat.
+  # Then divides the total by the number of users in the group to determine the
+  # average experience level of a group of users.
+  # Return type: Array [int for average music experience, int for average clinical experience, int for general education]
   def get_demographic_stats
     users = self.users
     average_experience = [0,0,0]
