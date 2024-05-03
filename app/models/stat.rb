151b9f9c5d05f9d8e6d0e887b653c01b09cc8188
kmackin <kailey-mackin@uiowa.edu>
2024-05-02 23:40:29 -0500

    code comments for stats

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -11,8 +11,9 @@ class Stat < ActiveRecord::Base
   # Return type: None; updates model's progress level
   def set_progress_level
     user = User.find_by_id(self.user_id)
+    # Keys: type of experience, Values: strings w/ number of years of experience
     experience = {:mus => user.music_experience, :clin => user.clinical_experience, :gen_ed => user.general_education}
-    total_experience = 0
+    total_experience = 0 # sum of all years of experience
     experience.each_value do |value|
       if value.eql?("0")
         total_experience += 0
@@ -23,7 +24,7 @@ class Stat < ActiveRecord::Base
       else
         total_experience += 5
       end
-    end
+    end # assigns the user to an initial "progress level" for progress tracking
     if total_experience > 3
       progress_level = "intermediate"
     else
@@ -40,16 +41,17 @@ class Stat < ActiveRecord::Base
   # they are considered "intermediate", otherwise they are "beginner"
   # Return type: None; saves the new progress level for user everytime it's called and updates the scores
   def update_experience(new_scores)
+    # current score is a string formatted as "num_correct/num_total"; split by delimiter
     current_score = self.total_answers.split("/")
     correct_scores = current_score[0].to_i + new_scores[0]
     total_num = current_score[1].to_i + new_scores[1]
-    unless total_num.eql?(0)
+    unless total_num.eql?(0)  # avoid dividing by 0 & reassign progress level
       if (correct_scores.to_f * 100 / total_num).round(2) > 60
         self.progress_level = "intermediate"
       else
         self.progress_level = "beginner"
       end
-      self.total_answers = "#{correct_scores}/#{total_num}"
+      self.total_answers = "#{correct_scores}/#{total_num}" # add new scores to current scores
       self.save
     end
   end
